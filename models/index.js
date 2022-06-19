3365766ed545f89b036e390b55a5529a91b0321f
Ghulam Mustafa Zaka <g.mustafa.zaka@gmail.com>
2022-06-18 23:36:55 -0700

    adding tag route

--- models/index.js
+++ models/index.js
@@ -16,13 +16,13 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: "productTag_tag",
+  as: "productTag_product",
   foreignKey: "product_id",
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: "productTag_product",
+  as: "productTag_tag",
   foreignKey: "tag_id",
 });
 
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -10,7 +10,7 @@ router.get("/", (req, res) => {
       {
         model: Product,
         through: ProductTag,
-        as: "productTag_Product",
+        as: "productTag_tag",
       },
     ],
   })
@@ -30,6 +30,8 @@ router.get("/:id", (req, res) => {
     include: [
       {
         model: Product,
+        through: ProductTag,
+        as: "productTag_tag",
       },
     ],
   })
@@ -38,19 +40,63 @@ router.get("/:id", (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
-  // be sure to include its associated Product data
 });
 
+// create a new tag
 router.post("/", (req, res) => {
-  // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name,
+  })
+    .then((dbTagData) => res.json(dbTagData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
+// update a tag's name by its `id` value
 router.put("/:id", (req, res) => {
-  // update a tag's name by its `id` value
+  Tag.update(
+    {
+      tag_name: req.body.tag_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((dbTagData) => {
+      if (!dbTagData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
+// delete on tag by its `id` value
 router.delete("/:id", (req, res) => {
-  // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbTagData) => {
+      if (!dbTagData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
